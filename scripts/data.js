709a652df9424b4092f89e99b3d5204e5064ea29
Davina Leong <davina.leong.sy@gmail.com>
2022-09-08 12:08:52 +0800

    Added a sample project

---
+++ scripts/data.js
@@ -0,0 +1,13 @@
+const projects = [
+  {
+    name: "Intro section with dropdown navigation",
+    completedAt: "2022-07-28",
+    image: "fem-snap.png",
+    demoLink:
+      "https://davinaleong.github.io/fem-intro-section-with-dropdown-navgiation/",
+    repoLink:
+      "https://github.com/davinaleong/fem-intro-section-with-dropdown-navgiation",
+    challengeLink:
+      "https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5",
+  },
+]
