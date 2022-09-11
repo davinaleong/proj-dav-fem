9b4c92f7177e55d084ca1e4fa44f5dba14226713
Davina Leong <leong.shi.yun@gmail.com>
2022-09-11 21:48:02 +0800

    WIP updating data

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,18 @@
 const projects = [
   {
+    name: "Manage Landing Page",
+    completedAt: "2022-07-19",
+    image: "fem-manage.png",
+    demoLink:
+      "https://davinaleong.github.io/fem-manage-landing-page/",
+    repoLink:
+      "https://github.com/davinaleong/fem-manage-landing-page",
+    challengeLink:
+      "https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5",
+  },
+  {
     name: "Intro section with dropdown navigation",
-    completedAt: "2022-07-28",
+    completedAt: "2022-07-27",
     image: "fem-snap.png",
     demoLink:
       "https://davinaleong.github.io/fem-intro-section-with-dropdown-navgiation/",
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
 console.log(`main.js loaded`)
 
+// Declate elements
 const cardGridEl = document.querySelector(`[data-card-grid]`)
 
 const card = (project) => {
     const { name, completedAt, image, demoLink, repoLink, challengeLink } = project
+    const date = dayjs(completedAt).format(`DD MMM YYYY`)
+
     return `
     <div class="card | bg-neutral-800">
         <img src="./images/${image}" alt="${name} Image" class="card-image">
         <div class="card-body | p-400">
             <h2 class="card-heading | ff-heading fs-600 fw-bold mb-300">${name}</h2>
-            <p><em>${completedAt}</em></p>
+            <p><em>${date}</em></p>
         </div>
         <div class="card-footer">
             <a href="${demoLink}" class="link-primary" target="_blank">View<br>Demo</a>
@@ -22,7 +25,10 @@ const card = (project) => {
 
 let cardGridInnerHtml = ``
 
-projects.forEach(project => {
+// Sort projects by completed at in descending order
+const sortedProjects = projects.sort((a, b) => (a.completedAt < b.completedAt) ? 1 : -1)
+
+sortedProjects.forEach(project => {
     cardGridInnerHtml += card(project)
 })
 
